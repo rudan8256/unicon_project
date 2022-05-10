fc2ff720a2028fc3114bf278140fe649bcb33404
sonbe <sonbe8704@gmail.com>
2022-05-10 16:41:08 +0900

    [안승재] 마커변경완

--- app/src/main/java/com/example/unicon_project/MapTest.java
+++ app/src/main/java/com/example/unicon_project/MapTest.java
@@ -9,9 +9,13 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -19,21 +23,34 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.Layout;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
 
 public class MapTest extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
@@ -50,10 +67,14 @@ public class MapTest extends AppCompatActivity implements OnMapReadyCallback, Go
     private AppCompatActivity mActivity;
     boolean askPermissionOnceAgain =false;
     boolean mRequestingLocationUpdates = false;
+    private FusedLocationProviderClient mFusedLocationClient;
     Location mCurrentLocation;
     boolean mMoveMapByUser=true;
     boolean mMoveMapByApi = true;
     LatLng currentPosition;
+    ArrayList<Item> sampleList= new ArrayList<>();
+    private View marker_root_view;
+    private TextView tv_marker;
 
     LocationRequest locationRequest = new LocationRequest()
             .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
@@ -65,8 +86,19 @@ public class MapTest extends AppCompatActivity implements OnMapReadyCallback, Go
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_test);
 
+        //
         SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        //
+
+        //
+        LocationSettingsRequest.Builder builder =
+                new LocationSettingsRequest.Builder();
+
+        builder.addLocationRequest(locationRequest);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        //
 
         mActivity = this;
 
@@ -75,6 +107,8 @@ public class MapTest extends AppCompatActivity implements OnMapReadyCallback, Go
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+
+        //
     }
 
     @Override
@@ -230,13 +264,17 @@ public class MapTest extends AppCompatActivity implements OnMapReadyCallback, Go
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap=googleMap;
 
-        LatLng Seoul = new LatLng(37.56,126.97);
+        LatLng Seoul = new LatLng(37.560001,126.97);
 
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(Seoul);
         markerOptions.title("서울");
         markerOptions.snippet("한국의 수도");
         mMap.addMarker(markerOptions);
+
+        setCustomMarkerView();
+
+        getSampleMarkerList();
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Seoul,10));
 
@@ -260,5 +298,73 @@ public class MapTest extends AppCompatActivity implements OnMapReadyCallback, Go
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    //현재위치 받아오기
+
+    //implementation 'com.google.anddroid.matterial:com.google.android.material:materail:1.4.0-alpha-02'
+    private void getCurrentPosition(){
+
+
+    }
+
+    //마커 변경
+    private void setCustomMarkerView(){
+        marker_root_view = LayoutInflater.from(this).inflate(R.layout.marker,null);
+        tv_marker= marker_root_view.findViewById(R.id.tv_price);
+    }
+
+    private Bitmap createDrawableFromView(Context context, View view){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity)context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels,displayMetrics.heightPixels);
+        view.layout(0,0,displayMetrics.widthPixels,displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(),view.getMeasuredHeight(),Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+
+    private Marker addMarker(Item item, boolean isSelectedMarker){
+        LatLng position = item.getLatLng();
+        int price =item.getPrice();
+        String formatted = NumberFormat.getCurrencyInstance().format(price);
+        Log.e(TAG,"addMarker"+formatted);
+        tv_marker.setText(formatted);
+
+        if(isSelectedMarker){
+
+        }
+        else{
+
+        }
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title(Integer.toString(price));
+        markerOptions.position(position);
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this,marker_root_view)));
+
+        Log.e(TAG,"addMarker"+position);
+        return mMap.addMarker(markerOptions);
+    }
+
+    private void getSampleMarkerList(){
+        LatLng latlng = new LatLng(37.56,126.97);
+        sampleList.add(new Item(latlng,1000));
+        latlng = new LatLng(37.561,126.97);
+        sampleList.add(new Item(latlng,1000));
+        latlng = new LatLng(37.562,126.97);
+        sampleList.add(new Item(latlng,1000));
+        latlng = new LatLng(37.563,126.97);
+        sampleList.add(new Item(latlng,1000));
+
+        for(Item markerItem : sampleList){
+            Log.e(TAG,"sampleList"+markerItem.getLatLng());
+            addMarker(markerItem,false);
+        }
     }
 }
