43e40746dce1d3646421db75308dce06e68afbad
rudan <ruddd8256@naver.com>
2022-05-24 21:46:30 +0900

    추천 매물페이지 초안 [박경무]

--- app/src/main/java/com/example/unicon_project/Pages/RecommendPage.java
+++ app/src/main/java/com/example/unicon_project/Pages/RecommendPage.java
@@ -1,16 +1,111 @@
 package com.example.unicon_project.Pages;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
 
+import com.example.unicon_project.Adapters.SaleProductAdapter;
+import com.example.unicon_project.Classes.SaleProduct;
 import com.example.unicon_project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecommendPage extends AppCompatActivity {
+
+    private LinearLayout recommend_condition;
+    private RecyclerView recommend_list;
+    private List<SaleProduct> mDatas;
+    SaleProductAdapter saleProductAdapter;
+    FirebaseFirestore mStore = FirebaseFirestore.getInstance();
+    Dialog condition_dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend_page);
+
+        recommend_condition=findViewById(R.id.recommend_condition);
+        recommend_list=findViewById(R.id.recommend_list);
+
+
+        recommend_condition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 showDialog();
+            }
+        });
+
+        updateDatas();
+
+        condition_dialog= new Dialog(this);
+        condition_dialog.setContentView(R.layout.dialog_reccondition);
+        condition_dialog.setCanceledOnTouchOutside(true);
+
+
     }
+
+    public void updateDatas() {
+        mDatas = new ArrayList<>();//
+        mStore.collection("SaleProducts").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task!=null){
+                    mDatas.clear();
+                    Log.e("###","쿼리개수 : "+task.getResult().getDocuments().size());
+                    for(DocumentSnapshot snap : task.getResult().getDocuments()){
+
+                        SaleProduct curdata = snap.toObject(SaleProduct.class);
+                        if(Judge(curdata)) {mDatas.add(curdata);}
+                    }
+                }
+
+
+                saleProductAdapter = new SaleProductAdapter( mDatas);
+                recommend_list.setAdapter(saleProductAdapter);
+                saleProductAdapter.setOnItemClickListener(new SaleProductAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View v, int position) {
+                        Intent intent = new Intent(getApplicationContext(), SaleProductPage.class);
+                        intent.putExtra("select_data", mDatas.get(position));
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+    }
+
+    private boolean Judge(SaleProduct curdata){
+
+        return true;
+    }
+
+    private void showDialog() {
+        condition_dialog.show();
+
+
+
+
+        condition_dialog.findViewById(R.id.complete_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
 }
